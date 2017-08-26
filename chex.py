454a7082d9af7458f0de6759c8b16954e8ab03e1
Abhi Nellore <anellore@gmail.com>
2017-08-26 13:37:49 -0700

    starts ChexIndex class

--- chex.py
+++ chex.py
@@ -7,15 +7,62 @@ Indexes chess game states from one or more PGN files
 (https://github.com/spotify/annoy) so the user can search for game states
 similar to a game state they input as well as the games in which they're
 found.
+
+Requires https://pypi.python.org/pypi/python-chess, 
+https://github.com/spotify/annoy, and https://pypi.python.org/pypi/sqlitedict.
 """
 import chess
-
+from annoy import Annoyindex
+from sqlitedict import SqliteDict
 
 _help_intro = """chex is a search engine for chess game states."""
 
 def help_formatter(prog):
     """ So formatter_class's max_help_position can be changed. """
     return argparse.HelpFormatter(prog, max_help_position=40)
+
+def node_to_bitvector(node):
+    """ Converts chess module's node to bitvector game state representation.
+
+        node: game object of type chess.pgn.Game
+
+        Return value: binary vector of length 768 as Python list
+    """
+
+class ChexIndex(AnnoyIndex):
+    """ Manages game states from Annoy Index and SQL database. """
+
+    def __init__(self, chex_index, id_label='FICSGamesDBGameNo',
+                    first_indexed_move=10):
+        """ Number of dimensions is always 8 x 8 x 12; there are 6 black piece
+        types, six white piece types, and the board is 8 x 8."""
+        super(ChexIndex, self).__init__(768, metric='angular')
+        self.id_label = id_label
+        self.first_indexed_move = first_indexed_move
+        self.chex_index = chex_index
+        self.chex_sql = SqliteDict(
+                            os.path.join(self.chex_index, 'sqlite.idx'))
+
+    def add_game(node):
+        """ Adds game parsed by chess library to chex index.
+
+            node: game object of type chess.pgn.Game
+
+            No return value.
+        """
+        i = 0
+        while not node.is_end():
+            i += 1
+            node = node.variations[0]
+            if i < first_indexed_move: continue
+            
+
+    def save():
+        super(ChexIndex, self).save(
+                os.path.join(self.chex_index, 'annoy.idx')
+            )
+        self.chex_sql.commit()
+        self.chex_sql.close()
 
 if __name__ == '__main__':
     # Print file's docstring if -h is invoked
@@ -50,10 +97,18 @@ if __name__ == '__main__':
             default='FICSGamesDBGameNo',
             help='game ID label from metadata in PGN files'
         )
+    index_parser.add_argument('-x', '--chex-index', metavar='<dir>',
+            required=True, type=str,
+            help='directory in which to store chex index files'
+        )
     search_parser.add_argument('-p', '--pgn', metavar='<file>',
             required=True, type=str,
             help='PGN describing game with state to search for')
     search_parser.add_argument('-m', '--move', metavar='<int>',
             required=True, type=int,
             help='move number from PGN corresponding to state to search for')
+    index_parser.add_argument('-x', '--chex-index', metavar='<dir>',
+            required=True, type=str,
+            help='chex index directory'
+        )
     args = parser.parse_args()
